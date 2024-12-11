6b34ec694ffcb4ab48198b4d57f7d5e05eda9e45
helarye <helarye@gmail.com>
2024-12-11 09:49:51 +0200

    Add project files.

---
+++ QLab/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace QLab;
+public class Program
+
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Queue<int> q = new Queue<int>();
+        q.Insert(7);
+        q.Insert(10);
+        q.Insert(3);
+        Console.WriteLine(q);
+    }
+}
