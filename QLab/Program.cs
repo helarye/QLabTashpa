5f7b3533ce9f1470d6b2067aef4085092ca0d4d0
helarye <helarye@gmail.com>
2024-12-31 13:54:45 +0200

    with qCopy

--- QLab/Program.cs
+++ QLab/Program.cs
@@ -2,7 +2,7 @@
 public class Program
 
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         Queue<int> q = new Queue<int>();
@@ -10,5 +10,34 @@ public class Program
         q.Insert(10);
         q.Insert(3);
         Console.WriteLine(q);
+        Queue<int> qCopy = SetQCopy(q);
+        Console.WriteLine(q);
+        Console.WriteLine(qCopy);
+        q.Insert(17);
+        qCopy.Insert(13);
+        Console.WriteLine(q);
+        Console.WriteLine(qCopy);
     }
+
+    public static Queue<int> SetQCopy(Queue<int> q)
+    {
+        Queue<int> qCopy = new();
+        Queue<int> qTemp = new();
+        int currItem;
+        //פריקת התור המקורי ויצירת תור העתק ותור זמני
+        while (!q.IsEmpty())
+        {
+            currItem = q.Remove();
+            qTemp.Insert(currItem);
+            qCopy.Insert(currItem);
+        }
+        //שחזור התור המקורי
+        while (!qTemp.IsEmpty())
+        {
+            q.Insert(qTemp.Remove());
+        }
+        return qCopy;
+    }
+        
+
 }
